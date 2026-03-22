dfd5b094a47392bf8145660eab0ae37be0a574de
Tamás Varga <varga.tamas.pro@gmail.com>
2026-03-22 13:24:37 +0100

    feat: find earliest and latest datetime

--- fix_dates.py
+++ fix_dates.py
@@ -3,6 +3,9 @@ from os.path import isfile
 
 from exif_services import ExifServices
 
+import datetime as dt
+
+EXIF_DATE_FORMAT = "%Y:%m:%d %H:%M:%S"
 PHOTO_FILE_EXTENSIONS = ["jpg", "jpeg"]
 
 def get_file_extension(filename):
@@ -15,10 +18,19 @@ if __name__ == "__main__":
     print(f"Current directory contains {len(filenames)} files with any of the extensions {PHOTO_FILE_EXTENSIONS}.")
 
     filenames_to_process = []
+    earliest_datetime = None
+    latest_datetime = None
     for filename in filenames:
-        datetime, datetime_original, datetime_digitized = ExifServices.get_exif_dates(filename)
-        if datetime == datetime_original and datetime == datetime_digitized:
+        datetime_string, datetime_original, datetime_digitized = ExifServices.get_exif_dates(filename)
+        if datetime_string == datetime_original and datetime_string == datetime_digitized:
             filenames_to_process.append(filename)
+            datetime = dt.datetime.strptime(datetime_string, EXIF_DATE_FORMAT)
+            if not earliest_datetime or datetime < earliest_datetime:
+                earliest_datetime = datetime
+            if not latest_datetime or datetime > latest_datetime:
+                latest_datetime = datetime
         else:
             print(f"{filename} has different datetime among its exif metadata: datetime, datetime_original, datetime_digitized - will not process it")
     print(f"Current directory contains {len(filenames_to_process)} photo files where the datetime exif metadata are consistent.")
+    print(f"Earliest datetime in the photos: {earliest_datetime}")
+    print(f"Latest datetime in the photos: {latest_datetime}")
